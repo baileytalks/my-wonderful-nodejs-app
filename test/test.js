3a82e19795d4475694a6c619093bb72be1fcc70e
Bailey Kursar <bkursar@gmail.com>
2017-05-17 20:42:55 +0100

    Trying to add my first mocha test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+
+describe('Display filtered task', function() {
+  var tasks = [{id:1, desc:'task', done:true},
+               {id:2, desc:'task', done:true}
+               {id:3, desc:'task', done:true}
+               {id:4, desc:'task', done:false}
+               {id:5, desc:'task', done:false}
+               {id:6, desc:'task', done:false}
+               {id:7, desc:'task', done:false}
+               {id:8, desc:'task', done:false}
+               {id:9, desc:'task', done:false}
+               {id:10, desc:'task', done:false}];
+  it('should return 3 done tasks given an array of 10 with 3 done', function() {
+    assert.equal(tasks)
+  })
+})
