364fe0d155bdeb2aa0e001c76ad696cb0f68c544
Bailey Kursar <bkursar@gmail.com>
2017-05-17 21:02:51 +0100

    Added a passing test for filtering tasks by whether or not they are done

--- test/test.js
+++ test/test.js
@@ -1,17 +1,19 @@
+var filter = require('../tasks/filter');
+
 var assert = require('assert');
 
 describe('Display filtered task', function() {
   var tasks = [{id:1, desc:'task', done:true},
-               {id:2, desc:'task', done:true}
-               {id:3, desc:'task', done:true}
-               {id:4, desc:'task', done:false}
-               {id:5, desc:'task', done:false}
-               {id:6, desc:'task', done:false}
-               {id:7, desc:'task', done:false}
-               {id:8, desc:'task', done:false}
-               {id:9, desc:'task', done:false}
+               {id:2, desc:'task', done:true},
+               {id:3, desc:'task', done:true},
+               {id:4, desc:'task', done:false},
+               {id:5, desc:'task', done:false},
+               {id:6, desc:'task', done:false},
+               {id:7, desc:'task', done:false},
+               {id:8, desc:'task', done:false},
+               {id:9, desc:'task', done:false},
                {id:10, desc:'task', done:false}];
   it('should return 3 done tasks given an array of 10 with 3 done', function() {
-    assert.equal(tasks)
+    assert.equal(3, filter(tasks).length);
   })
 })
